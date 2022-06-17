2508f8a1dcba73488cc150d9143f1ebe7203a960
Prakash Dsouza <84118928+PRAKASH-1971@users.noreply.github.com>
2022-06-17 15:44:54 +0530

    Add files via upload

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react'
+import "./css/Navbar.css"
+import { Link} from 'react-router-dom'
+
+
+function Navbar() {
+    // const [show, setShow] = useState(false)
+
+    // useEffect(()=>{
+    //     window.addEventListener("scroll",() => {
+    //         if (window.scrollY > 150) {
+    //             setShow(true);
+    //         } else setShow (false);
+    //     });
+    //     return ()=>{
+    //         window.removeEventListener("scroll");
+    //     };
+    // }, []);
+    
+        return (
+            <div className="navv">
+                <div className='navv-linkss'>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/5/5a/Zee5-official-logo.jpeg" alt="Netflix Logo" className="nav-logo" />
+                <Link to='/'><h4>Home</h4></Link>
+                <Link to='/zee5/TVshows'><h4>TV Shows</h4></Link>
+                <Link to='/zee5/Movies'><h4>Movies</h4></Link>
+                <Link to='/zee5/Web-series'><h4>Web Series</h4></Link>
+                <Link to='/zee5/News'><h4>News</h4></Link>
+                </div>
+                <div className='navv-linkss'>
+                    <div className='input-div'>
+                    <i class="fas fa-search"></i>
+                    <input type="text" placeholder='Search for Movies ,Shows etc..'/>
+                    </div>
+                    <i class="fas fa-download"></i>                    
+                    <button className='login-button'>LOGIN</button>
+                    <button className='buy-button'>BUY PLAN</button>
+                    <i class="fas fa-align-justify"></i>
+                </div>
+    
+            </div>
+        )
+}
+
+export default Navbar
